c20a8ed203af5ba8bf0e18247a068f70ad9a31c2
Thomas Lillis <thomasglillis@gmail.com>
2015-11-03 22:26:06 -0700

    working prior

---
+++ Assignment7/helpers.py
@@ -0,0 +1,66 @@
+C_TRUE = 0.5
+
+S_TRUE_C_TRUE = .1
+S_TRUE_C_FALSE = .5
+
+R_TRUE_C_TRUE = .8
+R_TRUE_C_FALSE = .2
+
+W_TRUE_S_TRUE_R_TRUE = 0.99
+W_TRUE_S_TRUE_R_FALSE = 0.90
+W_TRUE_S_FALSE_R_TRUE = 0.90
+W_TRUE_S_FALSE_R_FALSE = 0.00
+
+def check(sample):
+    ret =  {}
+    if sample['c'] >= C_TRUE:
+        ret['c'] = True
+        s_test = S_TRUE_C_TRUE
+        r_test = R_TRUE_C_TRUE
+    else:
+        ret['c'] = False
+        s_test = S_TRUE_C_FALSE
+        r_test = R_TRUE_C_FALSE
+
+
+    if sample['s'] >= s_test:
+        ret['s'] = True
+    else:
+        ret['s'] = False
+
+    if sample['r'] > r_test:
+        ret['r'] = True
+    else:
+        ret['r'] = False
+
+    if ret['r'] == True and ret['s'] == True:
+        w_test = W_TRUE_S_TRUE_R_TRUE
+    if ret['r'] == True and ret['s'] == False:
+        w_test = W_TRUE_S_TRUE_R_FALSE
+    if ret['r'] == False and ret['s'] == True:
+        w_test = W_TRUE_S_FALSE_R_TRUE
+    if ret['r'] == False and ret['s'] == False:
+        w_test = W_TRUE_S_FALSE_R_FALSE
+
+    if sample['w'] >= w_test:
+        ret['w'] = True
+    else:
+        ret['w'] = False
+
+    return ret
+
+def getSamples():
+    samples_file = open('random_samples')
+    samples_strings = samples_file.read().split(',')
+    samples = []
+
+    for i in range(len(samples_strings)):
+        samples.append(float(samples_strings[i]))
+
+    print "SAMPLES: \n"
+    print samples
+    print ""
+    return samples
+
+
+
---
+++ Assignment7/prior.py
@@ -0,0 +1,71 @@
+import helpers
+
+print("Prior Sampling\n")
+
+raw_samples = helpers.getSamples()
+samples = []
+
+for i in range(len(raw_samples)):
+    if (i) % 4 == 0:
+        sample = {
+            "c": raw_samples[i],
+            "s": raw_samples[i+1],
+            "r": raw_samples[i+2],
+            "w": raw_samples[i+3]
+        }
+        samples.append(sample)
+
+test = []
+for sample in samples:
+    test.append(helpers.check(sample))
+
+
+#### P(c = true) ####
+count = 0.0
+for sample in test:
+    if sample['c'] == True:
+        count += 1
+
+value = count/len(test)
+print('P(c = true) = {0}'.format(value))
+
+
+#### P(c = true | r = true) ####
+value = 0.0
+count = 0.0
+count_total = 0.0
+for sample in test:
+    if sample['r'] == True:
+        if sample['c'] == True:
+            count += 1
+        count_total += 1
+
+value = count/count_total
+print('P(c = true | r = true) = {0}'.format(value))
+
+
+#### P(s = true | w = true) ####
+value = 0.0
+count = 0.0
+count_total = 0.0
+for sample in test:
+    if sample['w'] == True:
+        if sample['s'] == True:
+            count += 1
+        count_total += 1
+
+value = count/count_total
+print('P(s = true | w = true) = {0}'.format(value))
+
+#### P(s = true | c = true, w = true) ####
+value = 0.0
+count = 0.0
+count_total = 0.0
+for sample in test:
+    if sample['c'] == True and sample['w'] == True:
+        if sample['s'] == True:
+            count += 1
+        count_total += 1
+
+value = count/count_total
+print('P(s = true | c = true, w = true) = {0}'.format(value))
